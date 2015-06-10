a1c05b833b86e6fcc865aaf0fbddceb962f4dde3
jtib <juliette.tib@gmail.com>
2015-06-11 00:00:57 +0200

    Recherche des pics en mieux

--- recherche_pics_freq.m
+++ recherche_pics_freq.m
@@ -1,4 +1,4 @@
-function [] = recherche_pics_freq(spectre,fichier_audio)
+function [loc,pks,cgravite_spectral] = recherche_pics_freq(fichier_audio,spectre)
 
 close all;
 
@@ -29,36 +29,20 @@ title('Spectre normalisé du signal (avec fenêtre de Hanning)');
 result=20*log10(abs(niv))/val_max;
 % prendre le spectre jusqu'a 25000 Hz (un peu large pour certains) et le tracer
 result=result(1:round(20000*512/fs));
+freq_result=freq(1:length(result));
 figure()
-plot(freq(1:length(result)),result); % frequence
-freq_fond=251; %faut-il prendre le 440 du la auquel le piano est accorde ou le pic a 251 qui correspond a la note jouee ?
-P_str=[];
-npic=1; % numero de pic
-
-for fh=freq_fond:freq_fond:36*freq_fond % 36 pics pour couvrir a peu pres les 25000Hz 
-  ind=round(f1*512/fs+1); 
-  tab1=zeros(1,length(result));
-  tab1(ind-20:ind+20)=result(ind-20:ind+20);
-  [valpic, indpic ]=max(tab1); % indice ou pic
-  fpic=(indpic-1)*fs/512;
-  if (valpic<0)
-   valpic=0;
-  end
-  freqp(npic)=fpic;
-  amp(npic)=valpic;
-  npic=npic+1;
-  %concatener les pics et leurs frequences
-  P_str2=[num2str(valpic), ',', num2str(fpic)];
-  P_str=[P_str '\r\n' P_str2];
-
-end
+plot(freq_result,result); % frequence
+P=findpeaksG(freq_result,result,0,-0.5,5,5);
+loc=P(:,2);
+pks=P(:,3)';
 
 % Centre de gravite
-cgravite_spectral=sum(freqp.*amp)/sum(amp);
+cgravite_spectral=sum(loc'.*pks)/sum(pks);
 % calcul des tri-stimulus
-tristimulus1=amp(1)/sum(amp);% A1/sigma(Ak)
-tristimulus2=(amp(2)+amp(3)+amp(4))/sum(amp); % A2+A3+A4/sigma(Ak)
-tristimulus3=1-tristimulus1-tristimulus2; % sigma(Ak,k>4)/sigma(Ak)
+%tristimulus1=pks(1)/sum(pks);% A1/sigma(Ak)
+%tristimulus2=(pks(2)+pks(3)+pks(4))/sum(pks); % A2+A3+A4/sigma(Ak)
+%tristimulus3=1-tristimulus1-tristimulus2; % sigma(Ak,k>4)/sigma(Ak)
+
 
 end
 
